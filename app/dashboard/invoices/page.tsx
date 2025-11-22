bbe28416623a92a1825f8d47bb81df5b8df2f06c
andrei-zyl <19lasvegas86@gmail.com>
2025-11-22 17:51:05 +0300

    Completed 15 chapters

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,5 @@
 import Pagination from '@/app/ui/invoices/pagination';
+import { Metadata } from 'next';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
@@ -7,6 +8,10 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
  
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
